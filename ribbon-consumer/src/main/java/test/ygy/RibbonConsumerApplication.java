46b9081e247b65dbb88abd8f7b73b8af7c14b7c7
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-08-08 11:27:02 +0800

    ribbon  消费者配置 与consumerController编写，负载均衡获取eureka服务

--- ribbon-consumer/src/main/java/test/ygy/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/test/ygy/RibbonConsumerApplication.java
@@ -2,9 +2,21 @@ package test.ygy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RibbonConsumerApplication {
+
+
+	@Bean
+	@LoadBalanced     //负载均衡
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonConsumerApplication.class, args);
