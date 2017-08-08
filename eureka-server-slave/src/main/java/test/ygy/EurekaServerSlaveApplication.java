1b2d2f1fa87bca8284e2867fc92efb9978c3d8ee
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-08-08 10:47:03 +0800

    eureka  服务从节点，提供hello world服务

---
+++ eureka-server-slave/src/main/java/test/ygy/EurekaServerSlaveApplication.java
@@ -0,0 +1,12 @@
+package test.ygy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerSlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerSlaveApplication.class, args);
+	}
+}
