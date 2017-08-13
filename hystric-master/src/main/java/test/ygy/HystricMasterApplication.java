9eda52807d5ceacfa0c6017fcae181cad580cf60
ygy <ygyworkforit@163.com>
2017-08-13 22:34:56 +0800

    单应用仪表  引入pom

--- hystric-master/src/main/java/test/ygy/HystricMasterApplication.java
+++ hystric-master/src/main/java/test/ygy/HystricMasterApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+//@SpringCloudApplication
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker  // 开启容错处理
 @ServletComponentScan   //开启扫描filter
+@EnableHystrixDashboard   //开启仪表
 public class HystricMasterApplication {
 
 	@Bean
