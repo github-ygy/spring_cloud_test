f0f7ad3c0b659b285304752d1e0376e4236a0813
ygy <ygyworkforit@163.com>
2017-08-13 19:23:47 +0800

    eureka 提供批量查询的服务

--- eureka-client/src/main/java/test/ygy/controller/HelloController.java
+++ eureka-client/src/main/java/test/ygy/controller/HelloController.java
@@ -3,8 +3,12 @@ package test.ygy.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by guoyao on 2017/8/7.
@@ -29,5 +33,16 @@ public class HelloController {
         }
         log.info("  this is eureka client asyn server");
         return " hello ,i am eureka client server asyn" + key;
+    }
+
+    @RequestMapping("/getAllClient")
+    public List<String> getAllClient(String keys) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return Arrays.asList(keys.split(","));
     }
 }
