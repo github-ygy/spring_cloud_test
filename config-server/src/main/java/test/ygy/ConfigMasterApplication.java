16610aaf21ae2e0934ec3522009ef993829ec086
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-08-18 19:02:09 +0800

    cloud config server端快速入门

---
+++ config-server/src/main/java/test/ygy/ConfigMasterApplication.java
@@ -0,0 +1,14 @@
+package test.ygy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMasterApplication.class, args);
+	}
+}
