b41ab59b4fb29258a64e282177a2f278023ce227
ygy <ygyworkforit@163.com>
2017-11-15 22:08:32 +0800

    guaua  2 preconditions  checkArgument

--- study_guaua/src/main/java/com/test/guaua/day_guaua/Guaua_02_Preconditions.java
+++ study_guaua/src/main/java/com/test/guaua/day_guaua/Guaua_02_Preconditions.java
@@ -1,6 +1,7 @@
 package com.test.guaua.day_guaua;
 
 import com.google.common.base.Preconditions;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by guoyao on 2017/11/15.
@@ -8,11 +9,22 @@ import com.google.common.base.Preconditions;
 public class Guaua_02_Preconditions {
 
     public static void main(String[] args) {
-        testA(null,"不能为null");
+        //testA(null,"不能为null");
+        //testA("ygy","不能为null");
+        testB("","key不能为空或者null");
     }
 
+    //checkNotNull  判断是否为null，为null则抛出异常
     public static void testA(String key, String errorMsg) {
         Preconditions.checkNotNull(key, errorMsg);
     }
+
+    public static void testB(String key, String errorMsg) {
+        Preconditions.checkArgument(!StringUtils.isEmpty(key), errorMsg);
+    }
+
+
+
+
 
 }
