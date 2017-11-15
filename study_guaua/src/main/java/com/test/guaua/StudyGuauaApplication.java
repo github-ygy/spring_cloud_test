efded8c07671ed4ab5972f6cf4ab83e707c0cf9e
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-11-15 18:09:49 +0800

    guaua  1  optional

---
+++ study_guaua/src/main/java/com/test/guaua/StudyGuauaApplication.java
@@ -0,0 +1,12 @@
+package com.test.guaua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGuauaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGuauaApplication.class, args);
+	}
+}
